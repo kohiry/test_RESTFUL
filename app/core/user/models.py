75ca0e603211e5f8a17865dbaac179853dfd4b74
kohiry <kohiry@mail.ru>
2023-08-23 22:25:09 +0400

    upd README

--- app/core/user/models.py
+++ app/core/user/models.py
@@ -1,5 +1,13 @@
+"""
+Файл содержит модель пользователя для приложения.
+Модель представляет собой класс User, который имеет следующие атрибуты:
+
+- name: строка, представляющая имя пользователя
+- email: строка, представляющая адрес электронной почты пользователя
+- hashed_password: строка, представляющая пароль пользователя
+"""
 import uuid
-from datetime import datetime, time
+from datetime import datetime, timedelta
 
 import bcrypt
 import jwt
@@ -10,6 +18,10 @@ from sqlalchemy.orm import relationship
 
 
 class User(Base):
+    """
+    Класс пользователя, предоставляющий методы для хеширования пароля, проверки пароля и генерации токена.
+    """
+
     __tablename__ = "users"
 
     id = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4)
@@ -19,12 +31,39 @@ class User(Base):
     posts = relationship("Post", back_populates="author")
 
     def hashed_password(self, password: str):
+        """
+        Хеширует пароль пользователя.
+
+        Аргументы:
+        password (str): Пароль пользователя.
+
+        """
+
         self.hashed_password = bcrypt.hashpw(
             password.encode("utf-8"), bcrypt.gensalt().decode("utf-8")
         )
 
     def verify_password(self, password: str):
+        """
+        Проверяет пароль пользователя.
+
+        Аргументы:
+        password (str): Пароль пользователя.
+
+        Возвращает:
+        bool: True, если пароль правильный, иначе False.
+        """
+
         return bcrypt.checkpw(password.encode("utf-8"), self.hashed_password("utf-8"))
 
     def generate_token(self):
-        expiration =
+        """
+        Генерирует токен для пользователя.
+
+        Возвращает:
+        str: Токен пользователя.
+        """
+
+        expiration = datetime.utcnow() + timedelta(hours=24)
+        payload = {"sub": str(self.id), "exp": expiration}
+        return jwt.encode(payload, "", algorithm="HS256")  # maybe worong
